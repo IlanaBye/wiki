d88358ac6527513ba2d74e12767dc723355cadff
Ilana Bye <ilanabye@Daniels-MacBook-Pro.local>
2023-02-16 16:04:12 -0700

    set up

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const path = require("path");
+
+app.use(morgan("dev")); //logging middleware
+app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
+app.use(express.urlencoded({ extended: false })); //parsing middleware for form input data
+app.use(express.json());
