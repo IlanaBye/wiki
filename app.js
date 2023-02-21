a83145790976ea2d199a41f0068c095ac0bc6355
Ilana Bye <ilanabye@Daniels-MacBook-Pro.local>
2023-02-21 09:55:27 -0700

    add users and wiki

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ app.use(morgan("dev")); //logging middleware
 app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
 app.use(express.urlencoded({ extended: false })); //parsing middleware for form input data
 app.use(express.json());
+
+// app.use("/wiki", require("./routes/wiki")); //if '/wiki, go to ./routes/wiki to check that file - if the method and route match, do the thing
+// app.use("/users", require("./routes/users"));
+
+module.exports = app;
