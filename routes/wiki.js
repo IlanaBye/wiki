a213e46598dfa14b0029564d21eb709ac68d7114
Ilana Bye <ilanabye@Daniels-MacBook-Pro.local>
2023-02-23 14:49:31 -0700

    add page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { addPage } = require("../views");
+const { Page } = require("../models");
 
 router.get("/", (req, res, next) => {
   res.send("got to GET /wiki/");
 });
 
-router.post("/", (req, res, next) => {
-  res.send("got to POST /wiki/");
+router.post("/", async (req, res, next) => {
+  try {
+    const page = await Page.create({
+      title: "",
+      content: "",
+    });
+
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/add", (req, res, next) => {
