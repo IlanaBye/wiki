280a4392b9aec56c238eecbefbabb26db1359880
Ilana Bye <ilanabye@Daniels-MacBook-Pro.local>
2023-03-01 12:23:55 -0700

    added error page route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const { notFoundPage } = require("./views");
 // const wikiRouter = require('./routes/wiki');
 // const userRouter = require('./routes/users');
 
@@ -15,6 +16,10 @@ app.use("/wiki", require("./routes/wiki")); //if '/wiki, go to ./routes/wiki to 
 
 app.get("/", function (req, res) {
   res.redirect("/wiki/");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send(notFoundPage());
 });
 
 module.exports = app;
